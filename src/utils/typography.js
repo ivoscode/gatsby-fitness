bfc51e6ba63d5b4d03e45336519641815883b82c
Ivo <ilegzdins@yahoo.com>
2020-10-05 10:26:51 +0100

    contact form added

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,27 +1,25 @@
-import Typography from 'typography'
+import Typography from "typography"
 
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.44,
   scaleRatio: 2.15,
-  blockMarginBottom: 0.75,
+
   googleFonts: [
     {
       name: "Nunito",
       styles: ["400"],
     },
     {
-      name: "Raleway",
-      styles: ["400", "400i", "700", "700i"],
+      name: "Montserrat",
+      styles: ["400"],
     },
   ],
   headerFontFamily: ["Nunito", "sans-serif"],
-  bodyFontFamily: ["Raleway", "sans-serif"],
-  headerColor: "hsla(0,0%,0%,1)",
-  bodyColor: "hsla(0,0%,0%,0.8)",
+  bodyFontFamily: ["Montserrat", "sans-serif"],
   headerWeight: 400,
   bodyWeight: 400,
   boldWeight: 700,
 })
 
-export default typography;
+export default typography
